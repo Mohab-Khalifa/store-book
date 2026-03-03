dc0e0547084f9246a979a70ba5c66f6d756adce8
jimcousins <jim.r.cousins@gmail.com>
2026-03-03 15:01:48 +0000

    Added image to checkoutpage

--- script.js
+++ script.js
@@ -32,35 +32,3 @@ function bookSearch(e) {
         }
     }
 }   
-
-
-
-async function uploadImage(e) {
-    const imageContainer = document.querySelector("#imageContainer")
-    const imageSearch = "Game of thrones book cover"
-    try {
-        const imageResp = await fetch(`https://pixabay.com/api/?key=54809996-f9e360964dd93e3680f1ab46b&q=${imageSearch}&image_type=photo`)
-        if (imageResp.ok){
-            imageData = await imageResp.json()
-            console.log(imageData.hits[0].previewURL);
-            const li = document.createElement("li")
-            li.innerHTML = `<img src="${imageData.hits[0].previewURL}" alt="Picture of ${imageSearch}">`
-            imageContainer.appendChild(li)
-        }else{
-            throw `Error: http status code = ${imageResp.status}`
-        }
-    }catch (err){
-        console.log(err);
-    }
-}
-
-
- 
-function displayImage(e) {
-    const imageContainer = document.querySelector("#imageContainer")
-    const li = document.createElement("li")
-    // const bookName = e.target.bookName.innerHTML
-    const bookName = "Eragon"
-    li.innerHTML = `<img src="/Images/${bookName}Cover.jpg" alt="Picture of ${bookName} book cover.">`
-    imageContainer.appendChild(li)
-}
